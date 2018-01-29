8b27df6aef7b7f94e9e133a78f02e466b15a3a1b
RyAndrew <RyAndrew@github.com>
2018-01-29 01:03:37 -0600

    send data from websocket to process

--- app/controllers/servicesocket.go
+++ app/controllers/servicesocket.go
@@ -1,7 +1,14 @@
 package controllers
 
 import (
+	"bufio"
+	"bytes"
+	"container/list"
 	"fmt"
+	"io"
+	"net"
+	"os/exec"
+	"strings"
 	"time"
 
 	"github.com/revel/revel"
@@ -11,60 +18,200 @@ type ServiceSocket struct {
 	*revel.Controller
 }
 
+var processSocketData = make(chan []byte, 512)
+var processSocketActive = false
+var processSocketChecked = false
+
+var processSocketLocalLineBuffer = list.New()
+var activeConnectionsList = list.New()
+
 func init() {
-	fmt.Println("\n\n\nInitProcessSocket\n\n\n")
+	go initProcessSocket()
 }
 
+func initProcessSocket() {
+	fmt.Println("\nInitProcessSocket\n")
+	if processSocketChecked == true && processSocketActive == false {
+		launchProcessSocket()
+		time.Sleep(200 * time.Millisecond)
+	}
+	processSocketChecked = true
+
+	conn, err := net.Dial("tcp4", "127.0.0.1:25560")
+	if err != nil {
+		fmt.Println("\nSocket Connect Error! Trying again in 1 sec\n", err)
+		time.Sleep(time.Second)
+		initProcessSocket()
+		return
+	}
+	//timeoutTimer := time.NewTimer(time.Second * 4)
+	if !socketPizzaHandshake(conn) {
+		fmt.Println("Handshake Failed. Closing Connection")
+		conn.Close()
+		return
+	}
+	fmt.Println("\nSocket Connected!\n", err)
+	processSocketClosed := make(chan struct{})
+	processSocketActive = true
+
+	go func() {
+		fmt.Println("Process Connection Beginning to stream!")
+		scanner := bufio.NewScanner(conn)
+		for scanner.Scan() {
+			socketData := scanner.Bytes()
+			fmt.Printf("Read %d bytes from socket %s\n", len(socketData), conn.RemoteAddr())
+
+			fmt.Printf("%s\n", socketData)
+			processSocketLocalLineBuffer.PushBack(socketData)
+			sendToAllConnections(socketData)
+		}
+		err = scanner.Err()
+		if err != nil {
+			fmt.Println("Socket Scanner Error: ", err)
+		}
+		processSocketClosed <- struct{}{}
+	}()
+
+	for {
+		select {
+		case <-processSocketClosed:
+			processSocketActive = false
+			break
+		case msg := <-processSocketData:
+			withNewLine := append(msg, '\n')
+			conn.Write(withNewLine)
+		}
+	}
+
+}
+func launchProcessSocket() {
+	if processSocketActive == true {
+		return
+	}
+	fmt.Println("Launching Process!")
+	cmd := exec.Command("/home/miner/gocode/src/github.com/ryandrew/process-output-socket/process-output-socket")
+	err := cmd.Start()
+	if err != nil {
+		fmt.Println("Error launching Process Socket")
+	}
+	fmt.Println("Launched!")
+	processSocketActive = true
+}
+
+func socketPizzaHandshake(conn net.Conn) bool {
+	start := time.Now()
+
+	byteReader := bufio.NewReaderSize(conn, 2)
+	challengeBytes := make([]byte, 2)
+	challengeBytesExpected := []byte{'P', 'I'} //[]byte("PI")
+
+	_, err := io.ReadFull(byteReader, challengeBytes)
+	if err != nil {
+		fmt.Println("Pizza Handshake error 1.1: Socket Read Error!\n", err)
+		return false
+	}
+	if bytes.Compare(challengeBytesExpected, challengeBytes) != 0 {
+		fmt.Println("Pizza Handshake error 2.1: Invalid Challenge Expecting \"PI\"")
+		return false
+	}
+
+	conn.Write([]byte("ZZ\r"))
+
+	scanner := bufio.NewScanner(conn)
+	scanner.Scan()
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Pizza Handshake error 3: Error Waiting for challenge success\n", err)
+		return false
+	}
+
+	if strings.Compare(scanner.Text(), "PIZZA!") != 0 {
+		fmt.Println("Pizza Handshake error 4: Invalid challenge success. Expecting \"PIZZA!\"")
+		return false
+	}
+
+	elapsed := time.Since(start)
+	fmt.Printf("Pizza Handshake Complete. Elapsed: %s\n", elapsed)
+
+	return true
+}
+func sendToAllConnections(msg []byte) {
+	i := 0
+	for e := activeConnectionsList.Front(); e != nil; e = e.Next() {
+		i++
+		fmt.Println("Sending Data to ", i)
+		el := e.Value.(chan []byte)
+		el <- msg
+	}
+}
 func (c ServiceSocket) HandleWSConnection(ws revel.ServerWebSocket) revel.Result {
 
 	if ws == nil {
-		fmt.Println("invalid socket")
+		fmt.Println("Error 1 socket not valid")
 		return nil
 	}
 
-	OneSecChannel := time.NewTicker(time.Second * 1)
-	tickCount := 0
-	maxTicks := 10
-
+	fmt.Println("Sending old console output to client")
+	for e := processSocketLocalLineBuffer.Front(); e != nil; e = e.Next() {
+		str := e.Value.([]byte)
+		fmt.Printf("%s\n", str)
+		if ws.MessageSendJSON(string(str)) != nil {
+			fmt.Println("Error 2 sending to client")
+			break
+		}
+	}
+	messagesToSocket := make(chan []byte)
+	connectionListEntry := activeConnectionsList.PushBack(messagesToSocket)
 	//recieve messages into this channel
-	messagesFromSocket := make(chan string)
+	messagesFromSocket := make(chan []byte)
 	go func() {
-		var msg string
+		var msg []byte
 		for {
 			err := ws.MessageReceiveJSON(&msg)
 			if err != nil {
 				close(messagesFromSocket)
 				return
 			}
+			if strings.Compare(string(msg), "StartService") == 0 {
+				launchProcessSocket()
+				initProcessSocket()
+				continue
+			}
 			fmt.Println(msg)
-			messagesFromSocket <- msg
+			messagesFromSocket <- []byte(msg)
 		}
 	}()
+
+	// OneSecChannel := time.NewTicker(time.Second * 1)
+	// tickCount := 0
+	// maxTicks := 10
 
 	//do something with new messages
 	for {
 		select {
-		case tickTime := <-OneSecChannel.C:
-			tickCount++
-			if tickCount > maxTicks {
-				OneSecChannel.Stop()
-			}
-			if ws.MessageSendJSON(fmt.Sprintf("%3d", tickCount)+" "+tickTime.String()) != nil {
-				// socket disconnected
-				fmt.Println("Error 1 sending to client")
-				break
-			}
+		// case tickTime := <-OneSecChannel.C:
+		// 	tickCount++
+		// 	if tickCount > maxTicks {
+		// 		OneSecChannel.Stop()
+		// 	}
+		// 	if ws.MessageSendJSON(fmt.Sprintf("%3d", tickCount)+" "+tickTime.String()) != nil {
+		// 		// socket disconnected
+		// 		fmt.Println("Error 3 sending to client")
+		// 		break
+		// 	}
+		case msg := <-messagesToSocket:
+			ws.MessageSendJSON(string(msg))
 		case msg, ok := <-messagesFromSocket:
 			// If the channel is closed, they disconnected.
 			if !ok {
 				break
 			}
 
-			fmt.Printf("Recieved Message\n%s\n", msg)
-			ws.MessageSendJSON(msg)
+			fmt.Printf("WS Recieved Message\n%s\n", msg)
+			processSocketData <- msg
 		}
 	}
 
+	activeConnectionsList.Remove(connectionListEntry)
 	// if ws.MessageSendJSON("Done") != nil {
 	// 	// socket disconnected
 	// 	fmt.Println("Error 2 sending to client")
@@ -72,5 +219,5 @@ func (c ServiceSocket) HandleWSConnection(ws revel.ServerWebSocket) revel.Result
 
 	// data := make(map[string]interface{})
 	// data["success"] = true
-	// return c.RenderJSON(data)
+	return c.RenderJSON(nil)
 }
